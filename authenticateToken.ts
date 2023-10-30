67c1044f7c81c20b2c6c441090e2ba33eeb099f8
pondkarun <pondkarun@gmail.com>
2023-10-30 23:49:40 +0700

    log

--- authenticateToken.ts
+++ authenticateToken.ts
@@ -3,7 +3,7 @@ const authenticateToken = (req: any, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     const token: any = authHeader && authHeader.split(' ')[1]
    
-
+    console.log('1111 :>> ', 1111);
     next();
 }
 
