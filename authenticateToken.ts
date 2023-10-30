2cde064be5b912cdcfdba5e0fe4d194443adbc1d
pondkarun <pondkarun@gmail.com>
2023-10-30 23:44:41 +0700

    init

--- authenticateToken.ts
+++ authenticateToken.ts
@@ -1,31 +1,10 @@
 import { Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken'
-import jwt_decode from "jwt-decode";
-import config from '../config'
-
 const authenticateToken = (req: any, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     const token: any = authHeader && authHeader.split(' ')[1]
-    jwt.verify(token, config.JWT_SECRET ?? "", (err: any) => {
-        if (err) {
-            return res.sendStatus(403)
-        }
-        const model: any = jwt_decode(token),
-            dateNow: Date = new Date();
+   
 
-        req.token = token;
-        req.allkons = model.allkons;
-        req.usr_id = model.profile.usr_id;
-        req.created_by = model.profile.usr_id;
-        req.updated_by = model.profile.usr_id;
-        req.created_date = dateNow;
-        req.updated_date = dateNow;
-        req.allkons = model.allkons;
-
-        next()
-    })
-
-    // next();
+    next();
 }
 
 export default authenticateToken
